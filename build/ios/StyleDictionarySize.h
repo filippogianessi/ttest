2170d80fedcf65ba08a54bffc6c466a29e06bf01
Filippo Gianessi <filippogianessi@MBP-di-Filippo.station>
2024-04-03 19:12:04 +0200

    initialized styledictioinary

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Wed, 03 Apr 2024 17:11:31 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
